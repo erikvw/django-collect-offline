0dcbf8e3e95aa2846c03440d4bdd6b5c40f8414b
erikvw <ew2789@gmail.com>
2016-06-29 07:56:10 +0200

    show warning if role not defined

--- edc_sync/apps.py
+++ edc_sync/apps.py
@@ -1,11 +1,20 @@
+import sys
+
 from django.apps import AppConfig
 from django_crypto_fields.apps import DjangoCryptoFieldsAppConfig
+from django.core.management.color import color_style
 
 
 class EdcSyncAppConfig(AppConfig):
     name = 'edc_sync'
     verbose_name = 'Data Synchronization'
     role = None
+
+    def ready(self):
+        if not self.role:
+            style = color_style()
+            sys.stdout.write(style.NOTICE(
+                'Warning: Project uses \'edc_sync\' but has not defined a role for this app instance. See AppConfig.\n'))
 
 
 class DjangoCryptoFieldsApp(DjangoCryptoFieldsAppConfig):
