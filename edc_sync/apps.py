476dc9c66bb660f67f6106bea0841b2b439c2edc
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-08-06 19:04:31 +0200

    added file transfer attributes

--- edc_sync/apps.py
+++ edc_sync/apps.py
@@ -1,16 +1,34 @@
+import getpass
 import sys
+from django.conf import settings
 
 from django.apps import AppConfig as DjangoAppConfig
 from django.core.management.color import color_style
 
 
-class AppConfig(DjangoAppConfig):
+class EdcSyncAppConfig(DjangoAppConfig):
     name = 'edc_sync'
     verbose_name = 'Data Synchronization'
     role = 'server'
+
+    remote_server_ip = None
+    remote_username = 'django'
+    remote_dump_tx_files = '/Users/{}/transaction_json_files/dump'.format(remote_username)
+    remote_user_media_files = '/Users/{}/transaction_json_files/media/to_upload'.format(remote_username)
+
+    user_dump_tx_files = '/Users/{}/source/bcpp-interview/bcpp_interview/media/edc_sync/user_tx_files/dump'.format(getpass.getuser())
+    user_archived_tx_files = '/Users/{}/source/bcpp-interview/bcpp_interview/media/edc_sync/user_tx_files/archived'.format(getpass.getuser())
+
+    user_media_files = '/Users/{}/source/bcpp-interview/bcpp_interview/media/upload'.format(getpass.getuser())
+    location = None
 
     def ready(self):
         if not self.role:
             style = color_style()
             sys.stdout.write(style.NOTICE(
                 'Warning: Project uses \'edc_sync\' but has not defined a role for this app instance. See AppConfig.\n'))
+        if settings.EDC_SYNC_REMOTE_FILE_SERVER:
+            if not self.remote_server_ip or not self.remote_user_media_files:
+                style = color_style()
+                sys.stdout.write(style.NOTICE(
+                    'Warning: Project uses \'edc_sync file transfer\' but has not defined a required attributes for this app instance. See AppConfig.\n'))
