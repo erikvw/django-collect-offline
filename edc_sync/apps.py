2fbd926bb9878f6fe546e3497b905d01f3333c22
erikvw <ew2789@gmail.com>
2016-08-23 22:24:50 +0200

    update configparser import

--- edc_sync/apps.py
+++ edc_sync/apps.py
@@ -1,22 +1,22 @@
-import os
 import sys
 
 from django.apps import AppConfig as DjangoAppConfig
-from django.conf import settings
 from django.core.management.color import color_style
 
-from edc_base.config_parser_mixin import ConfigParserMixin
+from django_appconfig_ini.mixin import ConfigIniMixin
 
 style = color_style()
 
 
-class AppConfig(ConfigParserMixin, DjangoAppConfig):
+class AppConfig(ConfigIniMixin, DjangoAppConfig):
     name = 'edc_sync'
     verbose_name = 'Data Synchronization'
     role = 'server'
+    config_ini_attrs = {'edc_sync': ['role']}
 
     def ready(self):
         sys.stdout.write('Loading {} ...\n'.format(self.verbose_name))
+        self.overwrite_config_ini_attrs_on_class(self.name)
         if not self.role:
             sys.stdout.write(style.NOTICE(
                 ' Warning: Project uses \'edc_sync\' but has not defined a role for this '
