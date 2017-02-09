70d8bd48e0f8409361a4d494cb789947bad55064
erikvw <ew2789@gmail.com>
2017-02-09 17:47:52 +0200

    change url name

--- edc_sync/urls.py
+++ edc_sync/urls.py
@@ -12,14 +12,16 @@ router.register(r'outgoingtransaction', views.OutgoingTransactionViewSet)
 router.register(r'incomingtransaction', views.IncomingTransactionViewSet)
 
 urlpatterns = [
-    url(r'^api/transaction-count/$', views.TransactionCountView.as_view(), name='transaction-count'),
+    url(r'^api/transaction-count/$',
+        views.TransactionCountView.as_view(), name='transaction-count'),
     url(r'^api/', include(router.urls)),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_auth_token),  # will reply given username and password
+    # will reply given username and password
+    url(r'^api-token-auth/', obtain_auth_token),
     # url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'render/(?P<model_name>\w+)/(?P<pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/',
         views.RenderView.as_view(), name='render_url'),
     url(r'^jsreverse/$', urls_js, name='js_reverse'),
     url(r'^admin/', edc_sync_admin.urls),
-    url(r'^', views.HomeView.as_view(), name='home-url'),
+    url(r'^', views.HomeView.as_view(), name='home_url'),
 ]
