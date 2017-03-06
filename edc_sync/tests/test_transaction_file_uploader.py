2c3897cf80f8e7de913b5ba0a6626c177a8f5e5f
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-03-07 00:15:49 +0200

    tests for file uploader with correct sequence

---
+++ edc_sync/tests/test_transaction_file_uploader.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class TestTransactionFileUploader(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_transaction_file_valid_for_upload(self):
+        pass
