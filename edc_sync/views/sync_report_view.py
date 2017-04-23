7ce0f94839be71803155fe393b11fa73879b1784
erikvw <ew2789@gmail.com>
2017-04-23 14:24:12 +0200

    sync report

--- edc_sync/views/sync_report_view.py
+++ edc_sync/views/sync_report_view.py
@@ -1,35 +1,40 @@
 from edc_base.view_mixins import EdcBaseViewMixin
 
-from django.views.generic.base import TemplateView
 from django.apps import apps as django_apps
+from django.db.models.aggregates import Count
+from django.views.generic.base import TemplateView
 
-from edc_sync_files.classes import SyncReport
+from edc_sync_files.models import UploadTransactionFile
 
-from ..edc_sync_view_mixin import EdcSyncViewMixin
 from ..admin import edc_sync_admin
+from ..edc_sync_view_mixin import EdcSyncViewMixin
+from ..models import IncomingTransaction
 
 
-class SyncReportView(
-        EdcBaseViewMixin, EdcSyncViewMixin, TemplateView):
+class SyncReportView(EdcBaseViewMixin, EdcSyncViewMixin, TemplateView):
 
     template_name = 'edc_sync/sync_report.html'
 
-    def __init__(self, *args, **kwargs):
-        super(SyncReportView, self).__init__(*args, **kwargs)
+    @property
+    def report_data(self):
+        report_data = {}
+        qs = IncomingTransaction.objects.values('producer').filter(
+            is_consumed=False).annotate(Count('is_consumed')).order_by('producer')
+        for item in qs:
+            producer = item.get('producer')
+            report_data.update({producer: {
+                'total_not_consumed': item.get('is_consumed__count'),
+                'upload_transaction_file': UploadTransactionFile.objects.filter(
+                    producer=producer).order_by('-created').last()}})
+        return report_data
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        app_config = django_apps.get_app_config('edc_map')
+        app_config = django_apps.get_app_config('edc_sync')
         context.update(
             edc_sync_admin=edc_sync_admin,
             project_name=context.get(
                 'project_name') + ': ' + self.role.title(),
-            base_template_name=app_config.base_template_name)
+            base_template_name=app_config.base_template_name,
+            report_data=self.report_data)
         return context
-
-    def get(self, request, *args, **kwargs):
-        report = SyncReport()
-        context = self.get_context_data(**kwargs)
-        context.update({
-            'report_data': report.report_data})
-        return self.render_to_response(context)
