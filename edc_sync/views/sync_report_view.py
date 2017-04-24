66b69e2fbbae8a1c1062111577a5d1b0f5b94124
erikvw <ew2789@gmail.com>
2017-04-24 11:06:06 +0200

    add base template name

--- edc_sync/views/sync_report_view.py
+++ edc_sync/views/sync_report_view.py
@@ -1,5 +1,6 @@
 from edc_base.view_mixins import EdcBaseViewMixin
 
+from django.apps import apps as django_apps
 from django.db.models.aggregates import Count
 from django.views.generic import ListView
 
@@ -15,3 +16,9 @@ class SyncReportView(EdcBaseViewMixin, EdcSyncViewMixin, ListView):
         return IncomingTransaction.objects.values('producer').filter(
             is_consumed=False).annotate(
                 pending=Count('is_consumed')).order_by('producer')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        app_config = django_apps.get_app_config('edc_sync')
+        context.update(base_template_name=app_config.base_template_name)
+        return context
