5a29c2733adc099f9d5cd5c4a963717ae41ea0f8
erikvw <ew2789@gmail.com>
2017-04-24 11:02:04 +0200

    update sync report

--- edc_sync/views/sync_report_view.py
+++ edc_sync/views/sync_report_view.py
@@ -1,40 +1,17 @@
 from edc_base.view_mixins import EdcBaseViewMixin
 
-from django.apps import apps as django_apps
 from django.db.models.aggregates import Count
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
-from edc_sync_files.models import UploadTransactionFile
-
-from ..admin import edc_sync_admin
 from ..edc_sync_view_mixin import EdcSyncViewMixin
 from ..models import IncomingTransaction
 
 
-class SyncReportView(EdcBaseViewMixin, EdcSyncViewMixin, TemplateView):
+class SyncReportView(EdcBaseViewMixin, EdcSyncViewMixin, ListView):
 
     template_name = 'edc_sync/sync_report.html'
 
-    @property
-    def report_data(self):
-        report_data = {}
-        qs = IncomingTransaction.objects.values('producer').filter(
-            is_consumed=False).annotate(Count('is_consumed')).order_by('producer')
-        for item in qs:
-            producer = item.get('producer')
-            report_data.update({producer: {
-                'total_not_consumed': item.get('is_consumed__count'),
-                'upload_transaction_file': UploadTransactionFile.objects.filter(
-                    producer=producer).order_by('-created').last()}})
-        return report_data
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        app_config = django_apps.get_app_config('edc_sync')
-        context.update(
-            edc_sync_admin=edc_sync_admin,
-            project_name=context.get(
-                'project_name') + ': ' + self.role.title(),
-            base_template_name=app_config.base_template_name,
-            report_data=self.report_data)
-        return context
+    def get_queryset(self):
+        return IncomingTransaction.objects.values('producer').filter(
+            is_consumed=False).annotate(
+                pending=Count('is_consumed')).order_by('producer')
