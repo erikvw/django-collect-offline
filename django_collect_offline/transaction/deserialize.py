9517f6c420e21c98d8022b519dcd57da3b876aab
erikvw <ew2789@gmail.com>
2018-10-03 20:46:32 -0500

    use_natural_primary_keys=True

--- django_collect_offline/transaction/deserialize.py
+++ django_collect_offline/transaction/deserialize.py
@@ -6,9 +6,12 @@ def deserialize(json_text=None):
 
     Wraps django deserialize with defaults for JSON
     and natural keys.
+
+    See https://docs.djangoproject.com/en/2.1/topics/serialization/
     """
+
     return serializers.deserialize(
         "json", json_text,
         ensure_ascii=True,
         use_natural_foreign_keys=True,
-        use_natural_primary_keys=False)
+        use_natural_primary_keys=True)
